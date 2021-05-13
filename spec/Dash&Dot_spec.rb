fdb6e2d8e4aabff01de8f6ace5091b0333c19005
LuisSalazar <atalasturey@gmail.com>
2021-05-12 22:46:56 -0400

    avanzar 4 lados

--- spec/Dash&Dot_spec.rb
+++ spec/Dash&Dot_spec.rb
@@ -15,6 +15,15 @@ RSpec.describe "juego de Dash & Dot"do
     it "Deberia devolver su cordenada x" do
         expect(avanzar_oeste_este(2, "O")).to eq(1)
     end
+    it "Deberia devolver su cordenada x" do
+        expect(avanzar_oeste_este(2, "E")).to eq(3)
+    end
+    it "Deberia devolver su cordenada x" do
+        expect(avansar_norte_sur(2, "N")).to eq(1)
+    end
+    it "Deberia devolver su cordenada x" do
+        expect(avansar_norte_sur(2, "S")).to eq(3)
+    end
     #it "Deberia devolver la ubicacion inicial, final y comandos" do
     #    expect(Dash_Dot("5,5-2,2,N-IAIAIADA")).to eq("2,2,N-IAIAIADA-4,2,S")#[[],[],[]]
     #end
